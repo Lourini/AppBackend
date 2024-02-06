88ea4916f9f10f84c9d0f9fe4f7d11c1b1003af3
Othmane Lourini <61596340+Lourini@users.noreply.github.com>
2024-02-06 11:12:37 +0100

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,7 @@
 // db.js
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('fieldback', 'olourini', 'UCvG@h5yZkE5GfU', {
+const db = new Sequelize('fieldback', 'root', 'UCvG@h5yZkE5GfU', {
   host: '185.166.39.250',
   dialect: 'mysql',
   // Add other configurations as needed
